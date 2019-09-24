a3635776e50c901d5238cfcebc023476fa933453
Alex Eales <alex.eales@potatolondon.com>
2019-09-24 09:40:02 +0100

    Created interpreter struct and Command enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::convert::TryFrom;
+
+enum Command {
+    Increment,
+    Decrement,
+    ShiftRight,
+    ShiftLeft,
+    Output,
+    Input,
+    LoopStart,
+    LoopEnd,
+}
+
+impl TryFrom<char> for Command {
+    type Error = String;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '>' => Ok(Command::ShiftRight),
+            '<' => Ok(Command::ShiftLeft),
+            '+' => Ok(Command::Increment),
+            '-' => Ok(Command::Decrement),
+            '.' => Ok(Command::Output),
+            ',' => Ok(Command::Input),
+            '[' => Ok(Command::LoopStart),
+            ']' => Ok(Command::LoopEnd),
+            _ => Err(format!("Cannot convert {} to Command!", value)),
+        }
+    }
+}
+
+struct BrnFckInterpreter {
+    data_pointer: u16,
+    instruction_pointer: u16,
+    memory: [u8; 30000],
+}
+
+impl BrnFckInterpreter {
+    fn new() -> Self {
+        BrnFckInterpreter {
+            data_pointer: 0,
+            instruction_pointer: 0,
+            memory: [0; 30000]
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut interpreter = BrnFckInterpreter::new();
 }
