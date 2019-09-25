4f9e6b5b3997167f396cc5655711513f1c33224a
Alex Eales <alex.eales@potatolondon.com>
2019-09-25 08:29:13 +0100

    Started implementing loops

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ enum Command {
     ShiftLeft,
     Output,
     Input,
-    LoopStart,
+    LoopStart(usize),
     LoopEnd,
 }
 
@@ -23,7 +23,7 @@ impl TryFrom<char> for Command {
             '-' => Ok(Command::Decrement),
             '.' => Ok(Command::Output),
             ',' => Ok(Command::Input),
-            '[' => Ok(Command::LoopStart),
+            '[' => Ok(Command::LoopStart(0)),
             ']' => Ok(Command::LoopEnd),
             _ => Err(format!("Cannot convert {} to Command!", value)),
         }
@@ -33,6 +33,7 @@ impl TryFrom<char> for Command {
 struct BrnFckInterpreter {
     data_pointer: usize,
     instruction_pointer: usize,
+    loop_counter: usize,
     memory: [u8; 30000],
 }
 
@@ -41,6 +42,7 @@ impl BrnFckInterpreter {
         BrnFckInterpreter {
             data_pointer: 0,
             instruction_pointer: 0,
+            loop_counter: 0,
             memory: [0; 30000]
         }
     }
@@ -48,15 +50,26 @@ impl BrnFckInterpreter {
     fn execute(&mut self, source: String) {
         // Parse the source into commands.
         let mut commands: Vec<Command> = Vec::new();
-        for symbol in source.chars() {
-            commands.push(Command::try_from(symbol).unwrap());
+        // TODO: Maybe use a stack of loops and pop when one is closed to assign jump indexes.
+        for (idx, symbol) in source.chars().enumerate() {
+            let mut command = Command::try_from(symbol).unwrap();
+            match command {
+                Command::LoopStart(_) => {
+                    // Put onto stack and increment loop counter.
+                },
+                Command:: LoopEnd => {
+                    // Decrement loop counter, pop stack and assign jump index.
+                },
+                _ => {},
+            }
+            commands.push(command);
         }
         // Execute the commands.
         for command in commands {
             match command {
                 // TODO: Consider how we want to handle loops (maybe store the start/end in enum)?
                 //       This may be an extra step in the parsing phase, where loops are calculated.
-                Command::LoopStart => {},
+                Command::LoopStart(jump_idx) => {},
                 Command:: LoopEnd => {},
                 _ => self.execute_command(command),
             }
